f998888d96a3ff66824aeb800227efa3232b8d88
Alf Gaida <agaida@siduction.org>
2017-02-14 21:28:13 +0100

    fix root colors - the light blue might be to much

--- template/etc/profile.d/red_admin.sh
+++ template/etc/profile.d/red_admin.sh
@@ -1,3 +1,3 @@
 if [ "$(id -u)" -eq 0 ]; then
-  PS1='${debian_chroot:+($debian_chroot)}\[\033[01;31m\]\u\[\033[00m\]@\h:\[\033[01;34m\]\w\[\033[00m\]\$ '
+  PS1='${debian_chroot:+($debian_chroot)}\[\033[01;31m\]\u\[\033[00m\]@\h:\w\$ '
 fi
